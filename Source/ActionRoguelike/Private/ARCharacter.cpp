824fd0664d3828b55d3396ddcab585dda68f57a7
Heysircreative <heysircreative@gmail.com>
2022-12-27 13:08:14 +1100

    Lecture 5 Complete
    beginning of Lecture 6

--- Source/ActionRoguelike/Private/ARCharacter.cpp
+++ Source/ActionRoguelike/Private/ARCharacter.cpp
@@ -103,6 +103,8 @@ void AARCharacter::PrimaryAttack_TimeElapsed()
 	FActorSpawnParameters SpawnParams;
 	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
 
+	SpawnParams.Instigator = this;
+
 	GetWorld()->SpawnActor<AActor>(ProjectileClass, SpawnTM, SpawnParams);
 }
 
